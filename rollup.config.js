ab42639a73f68d1e438f6a63849569338bd121b3
perfectacle <perfectacle@naver.com>
2017-12-10 20:43:23 +0900

    rollup for browser

--- rollup.config.js
+++ rollup.config.js
@@ -3,33 +3,16 @@ import tslint from 'rollup-plugin-tslint';
 import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
-export default [
-    // Node and other module bulder for UMD build
-    {
-        input: 'src/index.ts',
-        output: {
-            file: pkg.main,
-            format: 'umd'
-        },
-        name: 'utils',
-        plugins: [
-            typescript(),
-            tslint()
-        ]
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: pkg.main,
+        format: 'iife'
     },
-
-    // browser-friendly IIFE build
-    {
-        input: 'src/index.ts',
-        output: {
-            file: pkg.browser,
-            format: 'iife'
-        },
-        name: 'utils',
-        plugins: [
-            typescript(),
-            tslint(),
-            uglify()
-        ]
-    },
-]
+    name: 'utils',
+    plugins: [
+        typescript(),
+        tslint(),
+        uglify()
+    ]
+}
