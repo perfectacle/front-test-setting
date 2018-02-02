8379769bd3c0f2fcd251db43bf173338caa9c340
yanggs <perfectacle@gmail.com>
2018-02-02 22:25:27 +0900

    Revert "asd (#4)"
    This reverts commit 945e6ddec6cc42d2410ba9b9e1cdf70f007eb3b8.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,18 @@
 import typescript from 'rollup-plugin-typescript2';
 import tslint from 'rollup-plugin-tslint';
+import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default {
     input: 'src/index.ts',
     output: {
         file: pkg.main,
-        format: 'cjs'
+        format: 'iife'
     },
+    name: 'utils',
     plugins: [
         typescript(),
-        tslint()
+        tslint(),
+        uglify()
     ]
 }
